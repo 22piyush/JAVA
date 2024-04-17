c9b9fd9e87a6a346be176706de8b9d18b82e15bd
22piyush <piyushaglawe05@gmail.com>
2024-04-17 09:20:29 +0530

    factorial program done

---
+++ program.java
@@ -0,0 +1,16 @@
+
+public class program {
+ 
+    public static void main(String[] args) {
+        
+        int a=5;
+        int fact=1;
+        
+        for(int i = a; i>0 ;i--)
+        {
+            fact=fact*i;
+        }
+        System.out.println(fact);
+    }
+    
+}
