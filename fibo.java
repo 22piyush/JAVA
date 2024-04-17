69427fbdc165def8ef444acc78074ec1aa1b9023
22piyush <piyushaglawe05@gmail.com>
2024-04-17 10:02:11 +0530

    complete fiobo program

--- fibo.java
+++ fibo.java
@@ -1,3 +1,19 @@
 public class fibo {
 
+    public static void main(String[] args) {
+
+        int num = 10;
+        int a = 0;
+        int b = 1;
+        int c;
+
+        System.out.print(a + " " + b);
+        for (int i = 2; i <= num; i++)
+        {
+            c=a+b;
+            System.out.print(" "+c +" ");
+            a=b;
+            b=c;
+        }
+    }
 }
