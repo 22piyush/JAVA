561c0b2347acdb86c4442c5fa765c6399685aa2a
22piyush <piyushaglawe05@gmail.com>
2024-04-17 09:36:56 +0530

    java prime

---
+++ primenumber.java
@@ -0,0 +1,23 @@
+public class primenumber {
+
+    public static void main(String[] args) 
+    {
+        
+        int n=13;
+        boolean value=false;
+
+        if(n%2==0)
+        {
+            value=true;
+
+        }
+        if(value)
+    {
+          System.out.println("Prime Number");
+    }
+   else{
+    System.out.println("not prime");
+   }
+    }
+   
+}
