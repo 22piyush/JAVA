24496e9e85e616fdeb7045d20b803e7557f593dd
22piyush <piyushaglawe05@gmail.com>
2024-04-17 09:50:26 +0530

    some changes important in this program

--- primenumber.java
+++ primenumber.java
@@ -3,21 +3,26 @@ public class primenumber {
     public static void main(String[] args) 
     {
         
-        int n=13;
+        int n=9;
         boolean value=false;
 
-        if(n%2==0)
+        for(int i=2; i<=n;i++)
         {
-            value=true;
+            if(n%i==0)
+            {
+                value=true;
+                break;
 
+            }
         }
         if(value)
-    {
-          System.out.println("Prime Number");
+        {
+            System.out.println("prime number");
+        }
+       else{
+            System.out.println("not prime");
+        }
     }
-   else{
-    System.out.println("not prime");
-   }
-    }
+
    
 }
