76695ef4385916ec5510a18e4d63328d05e5bbc1
22piyush <piyushaglawe05@gmail.com>
2024-04-17 10:18:23 +0530

    done three

---
+++ findlargest.java
@@ -0,0 +1,21 @@
+public class findlargest {
+
+    public static void main(String[] args) {
+
+        int a=10 , b=20 , c=30;
+
+        if(a>c && a>b)
+        {
+            System.out.println("a is greater");
+        }
+        else if(b>c && b>a)
+        {
+            System.out.println("b is greater");
+        }
+      else if(c>a && c>b)
+        {
+            System.out.println("c is greater");
+        }
+
+    }
+}
---
+++ swap.java
@@ -0,0 +1,14 @@
+public class swap {
+
+    public static void main(String[] args) {
+
+        int a= 20;
+        int b=30;
+
+        int c=a;
+        a=b;
+        b=c;
+
+        System.out.println("a=" +  a +" " +"b="+b);
+    }
+}
