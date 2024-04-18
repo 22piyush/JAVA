a9f40a30555a5d551c65b098e0fae949551b08e0
22piyush <piyushaglawe05@gmail.com>
2024-04-18 11:45:04 +0530

    done

---
+++ twoSum.java
@@ -0,0 +1,19 @@
+public class twoSum {
+
+    public static void main(String[] args) {
+
+        int nums[]={6,7,5,2};
+        int target=9;
+
+        for(int i=0; i<nums.length-1;i++)
+        {
+            for(int j=i+1; j<nums.length;j++)
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                    System.out.println("["+i+","+j+"]");
+                }
+            }
+        }
+    }
+}
